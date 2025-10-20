0ea50e632f6ca4a8fcbb5d19ee7c43e6c539452f
Your name <ansommer@byu.edu>
2025-10-19 20:23:28 -0600

    Logout Tests

--- server/src/test/java/service/LogoutServiceTests.java
+++ server/src/test/java/service/LogoutServiceTests.java
@@ -1,7 +1,9 @@
 package service;
 
+import com.google.gson.JsonObject;
 import dataaccess.DataAccessException;
 import dataaccess.MemoryDataAccess;
+import datamodel.AuthData;
 import datamodel.UserData;
 import org.junit.jupiter.api.*;
 
@@ -14,5 +16,33 @@ public class LogoutServiceTests {
     void setUp() {
         dataAccess = new MemoryDataAccess();
     }
+
+    @Test
+    public void logoutTest() throws Exception {
+        var loginService = new LoginService(dataAccess);
+        var registerService = new RegisterService(dataAccess);
+        var logoutService = new LogoutService(dataAccess);
+        var user = new UserData("validUsername", "validPassword", "email@gmail.com");
+
+        registerService.register(user);
+        AuthData loginResult = loginService.login(user);
+        String authToken = loginResult.authToken();
+        var res = logoutService.logout(authToken);
+
+        assertNotNull(res); //technically?
+        assertFalse(dataAccess.authExists(authToken));
+    }
+
+    @Test
+    public void logoutTestNoUser() throws Exception {
+        var logoutService = new LogoutService(dataAccess);
+        var user = new UserData("validUsername", "validPassword", "email@gmail.com");
+
+        String authToken = "1234";
+        UnauthorizedException e = assertThrows(UnauthorizedException.class, () -> logoutService.logout(authToken));
+        assertEquals("Error: unauthorized", e.getMessage());
+
+    }
+
 
 }
