519108450854afd92292cb3393bcf0de9fd0a680
Your name <ansommer@byu.edu>
2025-10-19 18:32:58 -0600

    Fixed the line that was too long

--- server/src/test/java/Service.java
+++ server/src/test/java/Service.java
@@ -41,8 +41,8 @@ public class Service {
     public void registerTestAlreadyTaken() throws Exception {
         var registerService = new RegisterService(dataAccess);
         registerService.register(new UserData("validUsername", "validPassword", "email@gmail.com"));
-        DataAccessException e = assertThrows(DataAccessException.class, () -> registerService.register(new UserData("validUsername", "pass", "email2@gmail.com")));
-
+        UserData duplicateUSer = new UserData("validUsername", "pass", "email2@gmail.com");
+        DataAccessException e = assertThrows(DataAccessException.class, () -> registerService.register(duplicateUSer));
         assertEquals("Error: already taken", e.getMessage());
     }
 
