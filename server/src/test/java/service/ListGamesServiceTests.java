e8b87faa424462f4eb7f32f59535150c92d78146
Your name <ansommer@byu.edu>
2025-10-19 20:49:26 -0600

    List Games Tests

--- server/src/test/java/service/ListGamesServiceTests.java
+++ server/src/test/java/service/ListGamesServiceTests.java
@@ -2,6 +2,8 @@ package service;
 
 import dataaccess.DataAccessException;
 import dataaccess.MemoryDataAccess;
+import datamodel.AuthData;
+import datamodel.GameData;
 import datamodel.UserData;
 import org.junit.jupiter.api.*;
 
@@ -14,4 +16,37 @@ public class ListGamesServiceTests {
     void setUp() {
         dataAccess = new MemoryDataAccess();
     }
+
+    @Test
+    public void listGamesTest() throws Exception {
+        var createGameService = new CreateGameService(dataAccess);
+        var listGamesService = new ListGamesService(dataAccess);
+        var registerService = new RegisterService(dataAccess);
+        var user = new UserData("validUsername", "validPassword", "email@gmail.com");
+
+        AuthData registerResult = registerService.register(user);
+        String authToken = registerResult.authToken();
+        createGameService.createGame(authToken, "AwesomeGame");
+
+        var res = listGamesService.listGames(authToken);
+        assertNotNull(res);
+        assertNotNull(dataAccess.getGames());
+    }
+
+    @Test
+    public void listGamesUnauthorizedTest() throws Exception {
+        //var createGameService = new CreateGameService(dataAccess);
+        var listGamesService = new ListGamesService(dataAccess);
+        //var registerService = new RegisterService(dataAccess);
+        //var user = new UserData("validUsername", "validPassword", "email@gmail.com");
+
+        //AuthData registerResult = registerService.register(user);
+        String authToken = "badAuth";
+        //createGameService.createGame(authToken, "AwesomeGame");
+
+        UnauthorizedException e = assertThrows(UnauthorizedException.class, () -> listGamesService.listGames(authToken));
+        assertEquals("Error: unauthorized", e.getMessage());
+        
+    }
+
 }
