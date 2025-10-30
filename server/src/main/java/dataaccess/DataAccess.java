47168549be685c6c071a3bef9f61a2e12e484207
Your name <ansommer@byu.edu>
2025-10-29 20:50:49 -0600

    So now I understand how this was supposed to be. Made it so it had all the MemoryDataAccess functions

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -1,9 +1,35 @@
 package dataaccess;
 
+import datamodel.AuthData;
+import datamodel.GameData;
 import datamodel.UserData;
+
+import java.util.HashMap;
 
 public interface DataAccess {
     void saveUser(UserData user);
 
     void getUser(String username);
+
+    String getUserFromAuthToken(String authToken);
+
+    HashMap<String, GameData> getGames();
+
+    GameData getOneGame(int gameID);
+
+    int getNextGameId();
+
+    void createGame(GameData game);
+
+    void deleteAuth(String auth);
+
+    void saveAuth(AuthData auth);
+
+    boolean authExists(String auth);
+
+    String getPass(String username);
+
+    boolean userExists(String username);
+
+    void clear();
 }
